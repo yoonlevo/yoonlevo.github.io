fcaab4e6bf1e71ed9a6e1dfac5d13840f46b3aa6
yoonlevo-desktop <32999999+yoonlevo@users.noreply.github.com>
2017-10-22 14:57:49 +0200

    gegs

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1,4 +1,3 @@
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.info">Mobirise</a> Mobirise v4.3.4';document.body.insertBefore(a,document.body.childNodes[0])}}();
 (function($) {
 
     var isBuilder = $('html').hasClass('is-builder');
@@ -1266,3 +1265,4 @@
         }
     }
 })(jQuery);
+!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.info">Mobirise</a> Mobirise v4.3.4';document.body.insertBefore(a,document.body.childNodes[0])}}();
